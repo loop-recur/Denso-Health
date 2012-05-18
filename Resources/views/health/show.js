775ba47c7c748d8dd49cf9b805f3f3d63f810e25
ryan charette <ryan@looprecur.com>
2012-05-17 23:38:45 -0700

    Added dialog message for guide, added back button from health show page to health index

--- Resources/views/health/show.js
+++ Resources/views/health/show.js
@@ -63,6 +63,15 @@ Views.health.show = function(healthItem) {
         backgroundImage: '/images/health_wiper_focus_poi_section_all.png',
         height: 199,
         width: 324
+      }),
+    
+      backButton = Ti.UI.createButton({
+        width: 75,
+        height: 25,
+        backgroundColor: 'white',
+        top: 65,
+        right: '21%',
+        zIndex: 100
       });
      
   itemFocus.add(itemLabel);
@@ -83,7 +92,14 @@ Views.health.show = function(healthItem) {
       message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat lorem id diam molestie non tincidunt nulla blandit. Fusce et dui ante. Sed velit eros, adipiscing vel viverra eget, tincidunt ut metus. Sed in fringilla libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris blandit aliquam ante sed laoreet. Duis quis urna lacus. Phasellus vehicula suscipit commodo. Donec rutrum, urna sed egestas rutrum, nunc diam tempus felis, at rutrum sem urna ac metus. Nunc pretium cursus nulla in luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris quis erat mi. Aliquam erat volutpat. Sed vel diam vel ipsum aliquet ullamcorper ut semper libero."}).show();
   });
        
+  backButton.addEventListener('click', function() {
+    Layouts.application.contentRightView.clear();
+    Layouts.application.contentRightView.clear();
+    Controllers.health.index();
+  });
+
   wiperFluidPoi.add(wiperFluidPoiInner);
+  Layouts.application.contentRightView.add(backButton);
   Layouts.application.contentRightView.add(wiperFluidPoi);
   Layouts.application.contentRightView.add(itemFocus);
 };
