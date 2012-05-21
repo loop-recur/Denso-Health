3c9a98f8e19268dafdff3b1168c0b83038696f2e
Taylor Smith <tjsmith19@gmail.com>
2012-05-21 11:17:18 -0700

    background fix

--- Resources/app.js
+++ Resources/app.js
@@ -3,8 +3,12 @@ Ti.include('/initializers/init.js');
 
 
 var win = Titanium.UI.createWindow({  
-      backgroundImage: '/images/main_bg_tan_leather_ptrn.png'
-    });
+	backgroundImage: '/images/main_bg_tan_leather_ptrn_full.png',
+	height:768,
+	width:1024,
+	top:0,
+	left:0
+});
 
 PromptConnect(function(ip){
 	SocketIp = ip;
